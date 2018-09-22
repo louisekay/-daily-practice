eb7bab16f42cb66b362bce36ae608958c9ee5d91
louisekay <39895176+louisekay@users.noreply.github.com>
2018-09-22 19:42:41 +0800

    2018.9.22
    改了3天，终于改完了。（其实只是每天改一个小时）

--- 循环链表操作.cpp
+++ 循环链表操作.cpp
@@ -17,7 +17,7 @@ typedef struct out
 }Out;
 
 
-Out * circle_list_create()//åˆ›å»ºé“¾è¡¨ç»“ç‚¹
+Out * circle_list_create()//´´½¨Á´±í½áµã
 {
 	Out * member = (Out *)malloc(sizeof(Out));
 	member->node.length = 0;
@@ -25,45 +25,54 @@ Out * circle_list_create()//åˆ›å»ºé“¾è¡¨ç»“ç‚¹
 	member->node.slider = 0;
 	return member;
 }
-//å°¾æ’æ³•æ’å…¥ä¸€ä¸ªç»“ç‚¹
+//Î²²å·¨²åÈëÒ»¸ö½áµã
 int circle_list_insert(Out *list, Out *t1)
 {
 	int i = 0;
 
 	if (list == NULL)
 	{
-		printf("circle_list_insert funtioné”™è¯¯\n");
+		printf("circle_list_insert funtion´íÎó\n");
 		return -1;
 	}
 
 
-	Out * current = list;//æš‚å­˜å¤´ç»“ç‚¹ï¼Œç›¸å½“äºæŒ‡æ ‡
+	Out * current = list;//Ôİ´æÍ·½áµã£¬Ïàµ±ÓÚÖ¸±ê
 	Out * t = (Out *)malloc(sizeof(Out));
-	t->num1 = t1->num1;//æŠŠä¼ è¿‡æ¥çš„ä¿¡æ¯æ‹·è´åˆ°å †åˆ›å»ºçš„ç»“ç‚¹ä¸­
+	t->num1 = t1->num1;//°Ñ´«¹ıÀ´µÄĞÅÏ¢¿½±´µ½¶Ñ´´½¨µÄ½áµãÖĞ
 
-	//åªæœ‰ä¸€ä¸ªç»“ç‚¹æ—¶
+	//Ö»ÓĞÒ»¸ö½áµãÊ±
 	if (list->node.length == 0)
 	{
 		current->node.next = (In *)t;
-		list->node.length = 1;
+		list->node.length++;
+		t->node.slider == list->node.length;
 		t->node.next = (In *)t;
 	}
-	else//å¤šä¸ªç»“ç‚¹æ—¶
-	{		
-		t->node.next = list->node.next;//ä½œä¸ºæœ€åä¸€ä¸ªç»“ç‚¹ï¼Œå…ˆä¿å­˜å¼€å¤´ç»“ç‚¹
-		for (current = list; current->node.next != list->node.next;current = (Out *)current->node.next);//å†éå†åˆ°æœ€åä¸€ä¸ªç»“ç‚¹ï¼Œè¿æ¥æ’å…¥çš„ç»“ç‚¹
-		current->node.next = (In *)t;
-		list->node.length++;
-    }
-/*å¤´æ’æ³•
-	for (i = 0; i < list->node.length; i++)//æ‰¾åˆ°æœ€åä¸€ä¸ªç»“ç‚¹
+	else if (list->node.length > 0)//´óÓÚµÈÓÚÒ»¸ö½áµãÊ±
+	{
+		//ÏÈÈÃ²åÈëµÄÔªËØ±£´æµÚÒ»¸ö½áµãµÄÖ¸Õë
+		t->node.next = list->node.next;
+		//È»ºó±éÀúÕÒµ½×îºóÒ»¸ö½áµã
+		for (i = 0; i < list->node.length; i++)
+		{
+			current = (Out *)current->node.next;
+		}
+		//×îºóµÄÔªËØ±£´æ²åÈëÔªËØµÄÖ¸Õë
+		current->node.next = (In *)t;//µ½´ËÁ¬½ÓÍê³É
+
+		list->node.length++;//¸ÄºÃ³¤¶È
+		t->node.slider == list->node.length;
+	}
+/*Í·²å·¨(Í·²åÃ»µ÷ÊÔ¹ı£¬²»ÖªµÀ»á³öÏÖÊ²Ã´Çé¿ö)
+	for (i = 0; i < list->node.length; i++)//ÕÒµ½×îºóÒ»¸ö½áµã
 	{
 		current->node.slider++;
 		current = (Out *)current->node.next;
 	}
-	current->node.next = (In *)t;//å…ˆæŠŠæœ€åä¸€ä¸ªè·Ÿæ’å…¥çš„ç»“ç‚¹è¿æ¥
-	t->node.next = list->node.next;//ç„¶åæŠŠæ’å…¥çš„ç»“ç‚¹è·Ÿå¤´ç»“ç‚¹è¿èµ·æ¥
-	list->node.next = (In *)t;//æœ€åæ‰è®©å¤´ç»“ç‚¹ä¿å­˜æ’å…¥ç»“ç‚¹çš„åœ°å€
+	current->node.next = (In *)t;//ÏÈ°Ñ×îºóÒ»¸ö¸ú²åÈëµÄ½áµãÁ¬½Ó
+	t->node.next = list->node.next;//È»ºó°Ñ²åÈëµÄ½áµã¸úÍ·½áµãÁ¬ÆğÀ´
+	list->node.next = (In *)t;//×îºó²ÅÈÃÍ·½áµã±£´æ²åÈë½áµãµÄµØÖ·
 */
 	return 0;
 }
@@ -74,7 +83,7 @@ int circle_list_destroy(Out *list)
 	Out * current = NULL;
 	if (list == NULL)
 	{
-		printf("circle_list_destroy funtioné”™è¯¯\n");
+		printf("circle_list_destroy funtion´íÎó\n");
 		return -1;
 	}
 
@@ -89,18 +98,26 @@ int circle_list_destroy(Out *list)
 int main()
 {
 	Out *list = circle_list_create();
-	Out t1,t2;
+	Out t1,t2,t3,t4;
 	Out * current = list;
 	int i = 0;
 	t1.num1 = 1;
 	t2.num1 = 2;
-	circle_list_insert(list,&t1);
-	circle_list_insert(list, &t2);
+	t3.num1 = 3;
+	t4.num1 = 4;
 
-	for (current = (Out *)list->node.next; current->node.next != list->node.next; current = (Out *)current->node.next)
+	circle_list_insert(list, &t1);
+	circle_list_insert(list, &t2);
+	circle_list_insert(list, &t3);
+	circle_list_insert(list, &t4);
+
+	current = (Out *)current->node.next;//ÏÈºóÒÆÒ»ÏÂ
+	for (i = 0; i < 8; i++)
 	{
-		printf("%d\n",(Out *)current->num1);		
+		printf("%d\n",(Out *)current->num1);	
+		current = (Out *)current->node.next;		
 	}
+
 	circle_list_destroy(list);
 
 	system("pause");
